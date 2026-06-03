85af236bd6a98c9fb5c71477c4139466117108cd
Wqffles-com <wqffles@astrolink.cc>
2026-06-03 17:34:22 +0200

    chore(lang): migrated tools to lang

--- lang.md
+++ lang.md
@@ -4,4 +4,7 @@ You have access to a tool `run_code`. This allows you to run code in a custom pr
 The format for calling functions is `function_name(args)`. You do not need to use quotes to supply arguments (e.g., not send_message("123", "asdf"), but send_message(123, asdf))
 
 Currently, these functions exist:
-- send_message(channel_id, content): Sends a message in the specified Discord channel
+- send_message(channel_id, content): Sends a message in the specified Discord channel.
+- take_note(user_id, title, content): Creates a note on the specified user with the title and content.
+- get_note(user_id, title): Retrieves a note from a user.
+- get_notes(user_id): Retrieve the titles of all a user's notes.
--- src/interpreter.go
+++ src/interpreter.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	path2 "path"
 	"regexp"
 	"strings"
 )
@@ -16,6 +18,73 @@ var functions = map[string]func(*App, []string) (any, error){
 
 		recipient = strings.ReplaceAll(recipient, "\"", "")
 		return app.Discord.ChannelMessageSend(recipient, message)
+	},
+	"take_note": func(app *App, i []string) (any, error) {
+		if len(i) != 3 {
+			return nil, fmt.Errorf("take_note requires 3 arguments: user_id, title, content")
+		}
+
+		userId := i[0]
+		title := i[1]
+		content := i[2]
+
+		path := path2.Join("data", "notes", userId, fmt.Sprintf("%s.md", title))
+		err := os.MkdirAll(path2.Dir(path), os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, err
+		}
+
+		file.WriteString(content)
+
+		return map[string]any{
+			"status": "note saved",
+		}, nil
+	},
+	"get_note": func(app *App, i []string) (any, error) {
+		if len(i) != 2 {
+			return nil, fmt.Errorf("get_note requires 2 arguments: user_id and title")
+		}
+
+		path := path2.Join("data", "notes", i[0], fmt.Sprintf("%s.md", i[1]))
+		dat, err := os.ReadFile(path)
+		if os.IsNotExist(err) {
+			return map[string]any{
+				"notes": "user has no notes",
+			}, nil
+		}
+		if err != nil {
+			return nil, fmt.Errorf("read notes file: %w", err)
+		}
+
+		return map[string]any{
+			"note": string(dat),
+		}, nil
+	},
+	"get_notes": func(app *App, i []string) (any, error) {
+		if len(i) != 1 {
+			return nil, fmt.Errorf("get_notes requires 1 argument: user_id")
+		}
+
+		path := path2.Join("data", "notes", i[0])
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return nil, fmt.Errorf("read notes directory: %w", err)
+		}
+
+		fileNames := make([]string, 0)
+		for _, file := range files {
+			if !file.IsDir() {
+				fileNames = append(fileNames, strings.ReplaceAll(file.Name(), ".md", ""))
+			}
+		}
+
+		return map[string]any{
+			"notes": fileNames,
+		}, nil
 	},
 }
 
